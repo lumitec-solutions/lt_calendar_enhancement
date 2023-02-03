a7ebe3fe510cffa9f01c3485d02417886b4f4d19
CybroOdooDev <cybroodoo@gmail.com>
2023-02-03 14:35:12 +0530

    [solved issue meeting]

--- lt_calendar_enhancement/models/calendar_event.py
+++ lt_calendar_enhancement/models/calendar_event.py
@@ -5,10 +5,33 @@
 # See LICENSE file for full licensing details.
 ##############################################################################
 from odoo import models, api, _, fields
+from odoo.addons.calendar.models.calendar_event import Meeting
 
 
 class CalendarEvent(models.Model):
     _inherit = "calendar.event"
+
+    def default_get(self, fields):
+        # super default_model='crm.lead' for easier use in addons
+        if self.env.context.get('default_res_model') and not self.env.context.get('default_res_model_id'):
+            self = self.with_context(
+                default_res_model_id=self.env['ir.model']._get_id(self.env.context['default_res_model'])
+            )
+        defaults = super(Meeting, self).default_get(fields)
+        # support active_model / active_id as replacement of default_* if not already given
+        if 'res_model_id' not in defaults and 'res_model_id' in fields and \
+                self.env.context.get('active_model') and self.env.context['active_model'] != 'calendar.event':
+            defaults['res_model_id'] = self.env['ir.model']._get_id(self.env.context['active_model'])
+            defaults['res_model'] = self.env.context.get('active_model')
+        if 'res_id' not in defaults and 'res_id' in fields and \
+                defaults.get('res_model_id') and self.env.context.get('active_id'):
+            defaults['res_id'] = self.env.context['active_id']
+        if self._context.get('model_name') == 'crm.lead':
+            defaults['res_model_id'] = self._context.get('model_id')
+            defaults['res_id'] = self._context.get('res_id')
+        return defaults
+
+    Meeting.default_get = default_get
 
     @api.model
     def _default_location(self):
--- lt_calendar_enhancement/models/mail_activity.py
+++ lt_calendar_enhancement/models/mail_activity.py
@@ -6,11 +6,50 @@
 ##############################################################################
 from odoo import models, Command
 from collections import defaultdict
+from odoo.tools import is_html_empty
 
 
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
 
+    def action_create_calendar_event(self):
+        """On scheduling meeting through schedule activity the partner gets added to the representative"""
+        self.ensure_one()
+        representative_ids = self.ids
+        representative_ids.append(self.env.user.partner_id.id)
+        action = self.env["ir.actions.actions"]._for_xml_id("calendar.action_calendar_event")
+        action['context'] = {
+            'default_activity_type_id': self.activity_type_id.id,
+            'default_res_id': self.env.context.get('default_res_id'),
+            'default_res_model': self.env.context.get('default_res_model'),
+            'default_name': self.summary or self.res_name,
+            'default_description': self.note if not is_html_empty(self.note) else '',
+            'default_activity_ids': [(6, 0, self.ids)],
+
+        }
+        partners = self.env.user.partner_id
+        if self.res_model == 'res.partner' and self.res_id and self.res_id not in partners.ids:
+            action['context'] = {
+                'default_representative_ids': [(4, self.res_id), (4, self.env.user.partner_id.id)],
+            }
+
+        model = self.env[self.res_model].browse(self.res_id)
+        if self.res_model in ['crm.lead', 'helpdesk.ticket', 'sale.order', 'project.task'] and model.partner_id.id and model.partner_id.id not in partners.ids:
+            action['context'] = {
+                'default_representative_ids': [(4, model.partner_id.id), (4, self.env.user.partner_id.id)],
+            }
+        if self.res_model == 'event.event' and model.organizer_id.id and model.organizer_id.id not in partners.ids:
+            action['context'] = {
+                'default_representative_ids': [(4, model.organizer_id.id), (4, self.env.user.partner_id.id)],
+            }
+        if self.env.context.get('default_res_model') == 'crm.lead':
+            action['context'] = {
+                'model_id': self.env['ir.model']._get_id(self.env.context.get('default_res_model')),
+                'res_id': self.env.context.get('default_res_id'),
+                'model_name': self.env.context.get('default_res_model')
+
+            }
+        return action
 
     def _action_done(self, feedback=False, attachment_ids=None):
         """ Overrided to post feedback to the lognote of the meeting and also to prevent the saving of feedback to the description
